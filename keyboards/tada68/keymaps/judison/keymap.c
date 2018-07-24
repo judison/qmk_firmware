45435e8e1b830e804d4f90788f24c9df2c47ed96
Judison Oliveira Gil Filho <judison@gmail.com>
2018-07-24 15:04:07 -0300

    Judison's Layout

---
+++ keyboards/tada68/keymaps/judison/keymap.c
@@ -0,0 +1,58 @@
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BL 0
+#define _FL 1
+
+#define _______ KC_TRNS
+#define MY_CAPS LT(_FL,KC_CAPS)
+#define OS_LSFT OSM(MOD_LSFT)
+#define OS_RSFT OSM(MOD_RSFT)
+#define OS_RALT OSM(MOD_RALT)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  /* Keymap _BL: (Base Layer) Default Layer
+   * ,----------------------------------------------------------------.
+   * |~ ` | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  [|  ]|Backsp |Esc |
+   * |----------------------------------------------------------------|
+   * |Tab  |  '|  ,|  .|  P|  Y|  F|  G|  C|  R|  L|  /|  =|  \  |Del |
+   * |----------------------------------------------------------------|
+   * |CAPS   |  A|  O|  E|  U|  I|  D|  H|  T|  N|  S|  -|Return |PgUp|
+   * |----------------------------------------------------------------|
+   * |Shift   |  ;|  Q|  J|  K|  X|  B|  M|  W|  V|  Z|Shift | Up|PgDn|
+   * |----------------------------------------------------------------|
+   * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
+   * `----------------------------------------------------------------'
+   */
+[_BL] = LAYOUT_ansi(
+  KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_LBRC, KC_RBRC,KC_BSPC,KC_ESC, \
+  KC_TAB, KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,   KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLSH, KC_EQL, KC_BSLS,KC_DEL, \
+  MY_CAPS,KC_A,   KC_O,   KC_E,   KC_U,   KC_I,   KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   KC_MINS,         KC_ENT, KC_PGUP,  \
+  OS_LSFT,KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,   KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   OS_RSFT,         KC_UP,  KC_PGDN, \
+  KC_LCTL,KC_LGUI,KC_LALT,                KC_SPC,                         OS_RALT,MO(_FL),KC_RCTRL,KC_LEFT,KC_DOWN,KC_RGHT),
+
+  /* Keymap _FL: Function Layer
+   * ,----------------------------------------------------------------.
+   * |Esc|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |    |
+   * |----------------------------------------------------------------|
+   * |     |   |   |   |PSc|   |   |   |   |   |   |   |   |     |Ins |
+   * |----------------------------------------------------------------|
+   * |      |   |   |   |   |   |   |   |   |   |   |   |        |Hom |
+   * |----------------------------------------------------------------|
+   * |        |Bl-|BL |BL+|BRT|   |   |   |MUT|VU-|VU+|      |PgU|End |
+   * |----------------------------------------------------------------|
+   * |    |    |    |                       |   |   |    |Hom|PgD|End |
+   * `----------------------------------------------------------------'
+   */
+[_FL] = LAYOUT_ansi(
+  KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_DEL,_______,  \
+  _______,_______,_______,_______,KC_PSCR,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_INS, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_HOME, \
+  _______,BL_DEC, BL_TOGG,BL_INC, BL_BRTG,_______,_______,_______,KC_MUTE,KC_VOLD,KC_VOLU,_______,KC_PGUP,KC_END, \
+  _______,_______,_______,                 _______,               _______,_______,KC_RCTRL,KC_HOME,KC_PGDN,KC_END),
+};
+
---
+++ keyboards/tada68/keymaps/judison/readme.md
@@ -0,0 +1,27 @@
+# Judison's TADA68 layout
+
+* Dvorak layout
+* Breath light mode
+* Caps as fn key, if held
+* One Shot Shift
+* One Shot AltGr
+
+![](https://i.imgur.com/X1bfHKo.png)
+
+<!--- 
+http://www.keyboard-layout-editor.com/
+Raw Data:
+
+["~\n`","!\n1","@\n2","#\n3","$\n4","%\n5","^\n6","&\n7","*\n8","(\n9",")\n0","{\n[","}\n]",{w:2},"Backspace","Esc"],
+[{w:1.5},"Tab","\"\n'","<\n,",">\n.","P","Y","F","G","C","R","L","?\n/","+\n=",{w:1.5},"|\n\\","Delete"],
+[{t:"#000000\n#0000ff",w:1.75},"Caps Lock\nFn",{t:"#000000"},"A","O","E","U","I","D","H","T","N","S","_\n-",{w:2.25},"Enter","Pg Up"],
+[{t:"#000000\n#ff0000",w:2.25},"Shift\nOSM",{t:"#000000"},":\n;","Q","J","K","X","B","M","W","V","Z",{t:"#000000\n#ff0000",w:1.75},"Shift\nOSM",{t:"#000000"},"Up","Pg Dn"],
+[{w:1.25},"Ctrl",{w:1.25},"Win",{w:1.25},"Alt",{a:7,w:6.25},"",{t:"#000000\n#ff0000",a:4},"Alt\nOSM",{t:"#0000ff"},"Fn",{t:"#000000"},"Ctrl","Left","Down","Right"],
+[{a:5,w:16,d:true},"\nFunction Layer"],
+[{a:4},"Esc","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12",{w:2},"Delete",{a:7},""],
+[{w:1.5},"","","","",{a:4},"Pr.Scn",{a:7},"","","","","","","","",{w:1.5},"",{a:4},"Ins"],
+[{a:7,w:1.75},"","","","","","","","","","","","",{w:2.25},"",{a:4},"Home"],
+[{a:7,w:2.25},"",{a:4},"Bl -","Bl Tog","Bl +","Breath",{a:7},"","","",{a:4},"Mute","Vol Dn","Vol Up",{a:7,w:1.75},"",{a:4},"Pg Up","End"],
+[{a:7,w:1.25},"",{w:1.25},"",{w:1.25},"",{w:6.25},"","","","",{a:4},"Home","Pg Dn","End"]
+
+--->
